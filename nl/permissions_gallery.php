c374450d0280fd8db0869edfbb1a346baead5f0d
David <neurofibromatose@live.nl>
2015-01-30 13:55:33 +0100

    eerste vertaling

--- nl/permissions_gallery.php
+++ nl/permissions_gallery.php
@@ -1,12 +1,13 @@
 <?php
 /**
 *
-* permissions_gallery [English]
+* permissions_gallery [Dutch]
 *
 * @package phpBB Gallery
 * @version $Id$
 * @copyright (c) 2007 nickvergessen nickvergessen@gmx.de http://www.flying-bits.org
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
 *
 **/
 
@@ -27,8 +28,8 @@ $lang['permission_cat']['gallery'] = 'phpBB Gallery';
 
 // Adding the permissions
 $lang = array_merge($lang, array(
-	'acl_a_gallery_manage'		=> array('lang' => 'Can manage the phpBB Gallery adjustments',	'cat' => 'gallery'),
-	'acl_a_gallery_albums'		=> array('lang' => 'Can add/edit albums and permissions',		'cat' => 'gallery'),
-	'acl_a_gallery_import'		=> array('lang' => 'Can use the import-function',				'cat' => 'gallery'),
-	'acl_a_gallery_cleanup'		=> array('lang' => 'Can clean up the phpBB Gallery',			'cat' => 'gallery'),
+	'acl_a_gallery_manage'		=> array('lang' => 'Kan de phpBB Galerij instellingen beheren',	'cat' => 'gallery'),
+	'acl_a_gallery_albums'		=> array('lang' => 'Kan albums en permissies toevoegen/wijzigen',		'cat' => 'gallery'),
+	'acl_a_gallery_import'		=> array('lang' => 'Kan de importeer-functie gebruiken',	        'cat' => 'gallery'),
+	'acl_a_gallery_cleanup'		=> array('lang' => 'Kan de phpBB Galerij opruimen',			'cat' => 'gallery'),
 ));
