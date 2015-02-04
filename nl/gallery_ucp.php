f1901a7a806429c6343833f0e129477ef8eafdcd
David <neurofibromatose@live.nl>
2015-02-04 16:58:45 +0100

    Bijna klaar...
    Twijfel nog een beetje het creëren of aanmaken, heb nu creëren.
    
    "you get notified on" vertaald als "waar je notificaties van krijgt" moet misschien zijn "waar je op geabonneerd bent" weet niet precies in welke context het is...
    
    Moet parse vertaald worden, neem aan dat het gewoon soort werkwoord is wat je niet vertaald

--- nl/gallery_ucp.php
+++ nl/gallery_ucp.php
@@ -1,12 +1,13 @@
 <?php
 /**
 *
-* gallery_ucp [English]
+* gallery_ucp [Dutch]
 *
 * @package phpBB Gallery
 * @version $Id$
 * @copyright (c) 2007 nickvergessen nickvergessen@gmx.de http://www.flying-bits.org
 * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+* [Dutch] translated by Dutch Translators (https://github.com/dutch-translators)
 *
 **/
 
@@ -24,59 +25,59 @@ if (empty($lang) || !is_array($lang))
 }
 
 $lang = array_merge($lang, array(
-	'ACCESS_CONTROL_ALL'			=> 'Everyone',
-	'ACCESS_CONTROL_REGISTERED'		=> 'Registered users',
-	'ACCESS_CONTROL_NOT_FOES'		=> 'Registered users, except your foes',
-	'ACCESS_CONTROL_FRIENDS'		=> 'Only your friends',
+	'ACCESS_CONTROL_ALL'			=> 'Iedereen',
+	'ACCESS_CONTROL_REGISTERED'		=> 'Geregistreerde gebruikers',
+	'ACCESS_CONTROL_NOT_FOES'		=> 'Geregistreerde gebruikers, behalve je vijanden',
+	'ACCESS_CONTROL_FRIENDS'		=> 'Alleen je vrienden',
 	'ALBUMS'						=> 'Albums',
-	'ALBUM_ACCESS'					=> 'Allow access for',
-	'ALBUM_ACCESS_EXPLAIN'			=> 'You can use your %1$sFriends and Foes lists%2$s to control access to the album. However <strong>moderators</strong> can <strong>always</strong> access the album.',
-	'ALBUM_DESC'					=> 'Album Description',
-	'ALBUM_NAME'					=> 'Album Name',
-	'ALBUM_PARENT'					=> 'Parent Album',
-	'ATTACHED_SUBALBUMS'			=> 'Attached subalbums',
+	'ALBUM_ACCESS'					=> 'Sta toegang toe voor',
+	'ALBUM_ACCESS_EXPLAIN'			=> 'Je kan je %1$sVrienden en Vijanden lijst%2$s gebruiken om de toegang tot je album te regelen. Echter, <strong>moderators</strong> hebben <strong>altijd/strong> toegang tot je album.',
+	'ALBUM_DESC'					=> 'Album Beschrijving',
+	'ALBUM_NAME'					=> 'Album Naam',
+	'ALBUM_PARENT'					=> 'Hoofd Album',
+	'ATTACHED_SUBALBUMS'			=> 'Bijgevoegde subalbums',
 
-	'CREATE_PERSONAL_ALBUM'			=> 'Create personal album',
-	'CREATE_SUBALBUM'				=> 'Create subalbum',
-	'CREATE_SUBALBUM_EXP'			=> 'You may attach a new subalbum to your personal gallery.',
-	'CREATED_SUBALBUM'				=> 'Subalbum successful created',
+	'CREATE_PERSONAL_ALBUM'			=> 'Creëer  persoonlijk album',
+	'CREATE_SUBALBUM'				=> 'Creëer  subalbum',
+	'CREATE_SUBALBUM_EXP'			=> 'Je kan en nieuw subalbum toevoegen aan je persoonlijke galerij.',
+	'CREATED_SUBALBUM'				=> 'Subalbum succesvol aangemaakt',
 
-	'DELETE_ALBUM'					=> 'Delete Album',
-	'DELETE_ALBUM_CONFIRM'			=> 'Delete Album, with all attached subalbums and images?',
-	'DELETED_ALBUMS'				=> 'Albums successful deleted',
+	'DELETE_ALBUM'					=> 'Verwijder Album',
+	'DELETE_ALBUM_CONFIRM'			=> 'Verwijder Album, met alle bijgevoegde subalbums en afbeeldingen?',
+	'DELETED_ALBUMS'				=> 'Albums succesvol verwijderd',
 
-	'EDIT'							=> 'Edit',
-	'EDIT_ALBUM'					=> 'Edit album',
-	'EDIT_SUBALBUM'					=> 'Edit Subalbum',
-	'EDIT_SUBALBUM_EXP'				=> 'You can edit your albums here.',
-	'EDITED_SUBALBUM'				=> 'Album successful edited',
+	'EDIT'							=> 'Wijzig',
+	'EDIT_ALBUM'					=> 'Wijzig album',
+	'EDIT_SUBALBUM'					=> 'Wijzig Subalbum',
+	'EDIT_SUBALBUM_EXP'				=> 'Je kan je album hier wijzigen.',
+	'EDITED_SUBALBUM'				=> 'Album succesvol gewijzigd',
 
-	'GOTO'							=> 'Go To',
+	'GOTO'							=> 'Ga naar',
 
-	'MANAGE_SUBALBUMS'				=> 'Manage your subalbums',
-	'MISSING_ALBUM_NAME'			=> 'Please insert a name for the album',
+	'MANAGE_SUBALBUMS'				=> 'Beheer je subalbums',
+	'MISSING_ALBUM_NAME'			=> 'Voer een naam in voor het album',
 
-	'NEED_INITIALISE'				=> 'You don’t have a personal album yet.',
-	'NO_ALBUM_STEALING'				=> 'You are not allowed to manage the Album of other users.',
-	'NO_MORE_SUBALBUMS_ALLOWED'		=> 'You added your maximum of subalbums to your personal album',
-	'NO_PARENT_ALBUM'				=> '&laquo;-- no parent',
-	'NO_PERSALBUM_ALLOWED'			=> 'You don’t have the permissions create your personal album',
-	'NO_PERSONAL_ALBUM'				=> 'You don’t have a personal album yet. Here you can create your personal album, with some subalbums.<br />In personal albums only the owner can upload images.',
-	'NO_SUBALBUMS'					=> 'No Albums attached',
-	'NO_SUBSCRIPTIONS'				=> 'You didn’t subscribe to any image.',
+	'NEED_INITIALISE'				=> 'Je hebt nog geen persoonlijk album.',
+	'NO_ALBUM_STEALING'				=> 'Je bent niet bevoegd om albums van andere te beheren.',
+	'NO_MORE_SUBALBUMS_ALLOWED'		=> 'Je hebt het maximum aantal subalbums voor je persoonlijke album bereikt',
+	'NO_PARENT_ALBUM'				=> '&laquo;-- geen hoofd',
+	'NO_PERSALBUM_ALLOWED'			=> 'Je hebt geen permissies om een persoonlijk album te creëren',
+	'NO_PERSONAL_ALBUM'				=> 'Je hebt nog geen persoonlijk album. Hier kan je persoonlijke albums creëren, met een aantal subalbums.<br />In persoonlijke albums kan alleen de eigenaar afbeeldingen uploaden.',
+	'NO_SUBALBUMS'					=> 'Geen albums toegevoegd',
+	'NO_SUBSCRIPTIONS'				=> 'Je bent niet geabonneerd op een afbeelding.',
 
 	'PARSE_BBCODE'					=> 'Parse BBCode',
 	'PARSE_SMILIES'					=> 'Parse smilies',
 	'PARSE_URLS'					=> 'Parse links',
-	'PERSONAL_ALBUM'				=> 'Personal album',
+	'PERSONAL_ALBUM'				=> 'Persoonlijk album',
 
-	'UNSUBSCRIBE'					=> 'stop watching',
-	'USER_ALLOW_COMMENTS'			=> 'Allow users to comment your images',
+	'UNSUBSCRIBE'					=> 'Uitschrijven',
+	'USER_ALLOW_COMMENTS'			=> 'Sta gebruikers toe om op je afbeeldingen te reageren',
 
-	'YOUR_SUBSCRIPTIONS'			=> 'Here you see albums and images you get notified on.',
+	'YOUR_SUBSCRIPTIONS'			=> 'Hier kan je albums en afbeeldingen zien waar je notificaties van krijgt.',
 
-	'WATCH_CHANGED'					=> 'Settings stored',
-	'WATCH_COM'						=> 'Subscribe commented images by default',
-	'WATCH_NOTE'					=> 'This option only affects on new images. All other images need to be added by the “subscribe image“ option.',
-	'WATCH_OWN'						=> 'Subscribe own images by default',
+	'WATCH_CHANGED'					=> 'Instellingen opgeslagen',
+	'WATCH_COM'						=> 'Aboneer standaard op afbeeldingen met reacties',
+	'WATCH_NOTE'					=> 'Deze optie heeft alleen effect op nieuwe afbeeldingen. Alle andere afbeeldingen moeten worden toegevoegd door de “abonneer afbeelding“ optie.',
+	'WATCH_OWN'						=> 'Aboneer standaard op eigen afbeeldingen',
 ));
